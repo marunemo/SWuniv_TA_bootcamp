989853d3f197074e13052ede287dae1c126fa17c
marunemo <chys3697@gmail.com>
2024-08-19 13:29:52 +0900

    Fix: comma position error check

--- untitled1/src/Main.java
+++ untitled1/src/Main.java
@@ -4,32 +4,51 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         String number, change = "";
+        Boolean neg = false;
         Scanner sc = new Scanner(System.in);
         int error = 0;
-        int start;
 
         while(error < 3) {
+            change = "";
             number = sc.nextLine();
 
-            start = number.length() - 1;
+            neg = false;
             if(number.charAt(0) == '-') {
-                change = "-";
-                start -= 1;
+                neg = true;
+//                start -= 1;
             }
 
-            for(int i = start; i >= 0; i--) {
-                if(number.charAt(i) >= '0' && number.charAt(i) <= '9')
-                    change += number.charAt(i);
+            for(int i = 0; i <= (neg ? number.length() - 2 : number.length() - 1); i++) {
+                if(number.charAt(number.length() - i - 1) >= '0' && number.charAt(number.length() - i - 1) <= '9')
+                    change = number.charAt(number.length() - i - 1) + change;
                 else {
                     error++;
                     if(error == 3)
                         System.out.println("Error occured.");
                 }
 
-                if(i % 3 == 0)
-                    change += ',';
+                if(i % 3 == 2)
+                    change = ',' + change;
             }
+            if(change.charAt(0) == ',')
+                change = change.substring(1);
 
+//            for(int i = number.length() - 1; i >= (neg ? 1: 0); i--) {
+//                if(number.charAt(i) >= '0' && number.charAt(i) <= '9')
+//                    change = number.charAt(i) + change;
+//                else {
+//                    error++;
+//                    if(error == 3)
+//                        System.out.println("Error occured.");
+//                }
+//
+//                if(i % 3 == 0)
+//                    change = ',' + change;
+//            }
+
+            if(neg) {
+                System.out.print("-");
+            }
             System.out.println(change);
         }
 
